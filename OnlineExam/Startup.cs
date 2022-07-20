001b83ce7a37b97927204f582fbbe308220c1861
Zubair Alam <35332975+zubairalamsub@users.noreply.github.com>
2022-07-20 16:06:11 +0600

    Home Page Added

--- OnlineExam/Startup.cs
+++ OnlineExam/Startup.cs
@@ -55,6 +55,7 @@ namespace OnlineExam
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseDefaultFiles();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
